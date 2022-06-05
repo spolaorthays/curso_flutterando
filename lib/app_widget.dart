2e23d55922abea9fb4fd98883a95ca2013f55aee
spolaorthays <thays.spolaor@gmail.com>
2022-06-05 19:09:33 -0300

    primeiro commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: Text(
+            'Flutter: $count',
+            style: TextStyle(fontSize: 20),
+          ),
+          onTap: () {
+            //Esse método reconstroi todo o método build, atualizando o conteúdo da tela
+            setState(() {
+              count++;
+            });
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+      ),
+    );
+  }
+}
